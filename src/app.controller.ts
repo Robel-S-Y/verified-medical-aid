026ba35617db841433a9043d3c3bc4bcc9a301e2
Robel Yidenekal <robelyidenekal@gmail.com>
2025-11-23 08:43:19 +0300

    up add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
   @Get('/')
+  @Public()
   getRoot() {
     return {
       message: 'Success! Server is Up and Running.',
